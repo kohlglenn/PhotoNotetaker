07cec2f39b71119e083d81553ddbbbf31d929ade
kohlglenn <kohlglenn@gmail.com>
2021-05-01 17:49:21 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,78 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response, NextFunction } from 'express';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-// import passport from 'passport';
-// import { MONGODB_URI, SESSION_SECRET } from './util/secrets';
-// import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-// import { User, UserDocument } from './models/User';
-// const cors = require('cors');
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import { MONGODB_URI, SESSION_SECRET } from './util/secrets';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { User, UserDocument } from './models/User';
+const cors = require('cors');
 
-// import users from './routes/api/users';
-// import feed from './routes/api/feed';
-// import create from './routes/api/create';
+import users from './routes/api/users';
+import feed from './routes/api/feed';
+import create from './routes/api/create';
 
 // initialize configuration
 dotenv.config();
 const port = process.env.PORT;
 
 // Connect to MongoDB
-// mongoose
-//   .connect(MONGODB_URI as string, { useNewUrlParser: true })
-//   .then(() => console.log('MongoDB successfully connected'))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(MONGODB_URI as string, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch((err) => console.log(err));
 
 const app: Express = express();
 
-// // app.use((req: Request, res: Response, next: NextFunction) => {
-// //   console.log(req.headers);
-// //   next();
-// // });
-// // Middleware
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb' }));
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   console.log(req.headers);
+//   next();
+// });
 
-// app.use(passport.initialize());
-// passport.use(
-//   new JwtStrategy(
-//     {
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       secretOrKey: SESSION_SECRET
-//     },
-//     (jwt_payload, done) => {
-//       User.findById(jwt_payload.id)
-//         .then((user) => {
-//           if (user) {
-//             return done(null, user);
-//           }
-//           return done(null, false);
-//         })
-//         .catch((err) => console.log(err));
-//     }
-//   )
-// );
+// Middleware
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb' }));
 
-// app.options('*', cors());
-// app.use(cors());
+app.use(passport.initialize());
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: SESSION_SECRET
+    },
+    (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch((err) => console.log(err));
+    }
+  )
+);
+
+app.options('*', cors());
+app.use(cors());
 
 // define a route handler for the default home page
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1> Hello World </h1>');
 });
 
-// app.get(
-//   '/protected',
-//   passport.authenticate('jwt', { session: false }),
-//   (req: Request, res: Response) => {
-//     res.send(req.user);
-//   }
-// );
+app.get(
+  '/protected',
+  passport.authenticate('jwt', { session: false }),
+  (req: Request, res: Response) => {
+    res.send(req.user);
+  }
+);
 
-// // routes
-// app.use('/api/users', users);
-// app.use('/api/feed', feed);
-// app.use('/api/create', create);
+// routes
+app.use('/api/users', users);
+app.use('/api/feed', feed);
+app.use('/api/create', create);
 
 // start the express server
 app.listen(port, () => {
